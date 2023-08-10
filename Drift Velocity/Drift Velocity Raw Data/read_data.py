c9e74178a1092b3bfed3cb45eaa9b371bca9d9af
Michael Frood <132622476+Frood-for-Thought@users.noreply.github.com>
2023-08-10 13:47:18 -0400

    fixed filter function for velocity

--- Drift Velocity/Drift Velocity Raw Data/read_data.py
+++ Drift Velocity/Drift Velocity Raw Data/read_data.py
@@ -21,24 +21,34 @@ def get_titles(dataframe):
     :return: get the names for columns with recorded drift velocity,
     and the alpha value for the distribution of velocities
     '''
-    vel_data = Diffusion_Data.columns[3]
-    alpha_const = Diffusion_Data.columns.values[1]
+    vel_data = dataframe.columns[3]
+    alpha_const = dataframe.columns.values[1]
     return vel_data, alpha_const
 
 
-def filter_data_describe(dataframe, vel_data, alpha_const):
+def filter_data_describe(dataframe, vel_col, alpha_col):
     """
     This removes all the outliers which are 3*standard_deviation
     away from the mean, and then return a describe_dataframe.
     :param dataframe: Input dataframe.
-    :param vel_data: Velocity data point column title.
-    :param alpha_const: Alpha constant column title.
-    :return:After the inputted dataframe is filtered, group by all
+    :param vel_col: Velocity data point column title.
+    :param alpha_col: Alpha constant column title.
+    :return: After the inputted dataframe is filtered, group by all
     the alpha constants to sort the velocity data and apply .describe().
     """
-    # Filter out datapoints that are greater than 3xSD
-    dataframe = dataframe[np.abs(stats.zscore(dataframe[vel_data]) <= 3)]
-    return dataframe.groupby(alpha_const)[vel_data].describe()
+    # A list of filtered velocity values to be used.
+    filtered_vel = pd.Series(dtype='float64')
+    # Loop through all the alpha constants.
+    for x in dataframe.alpha.unique():
+        # Get the velocity values for current alpha constant.
+        alpha_vel_val = dataframe[vel_col].loc[dataframe[alpha_col] == x]
+        # Filter out all the values which are greater than 3*standard_deviation
+        alpha_vel_val = alpha_vel_val.loc[stats.zscore(np.abs(alpha_vel_val)) <= 3]
+        # Add accepted velocity values to the filtered_vel list.
+        filtered_vel = pd.concat([filtered_vel, alpha_vel_val])
+    # Update dataframe to only have velocity values that are in "filtered_vel".
+    dataframe = dataframe[dataframe[vel_col].isin(filtered_vel)]
+    return dataframe.groupby(alpha_col)[vel_col].describe(), dataframe
 
 
 def find_optimal_constant(describe_dataframe, dataframe, vel_data, lowest_tstat=1000, pvalue=0.05):
@@ -65,13 +75,13 @@ def find_optimal_constant(describe_dataframe, dataframe, vel_data, lowest_tstat=
 
 
 file = 'pos_30_alpha_values_MaxC_60000_Grad_0.000405.xlsx'
-Diffusion_Data = pd.DataFrame(get_data(file))
+Alpha_Vel_Data = pd.DataFrame(get_data(file))
 pd.set_option('display.max_columns', None)  # Show all the columns
-vel_data, alpha_const = get_titles(Diffusion_Data)
-describe_diff_by_const = filter_data_describe(Diffusion_Data, vel_data, alpha_const)
+vel_col, alpha_col = get_titles(Alpha_Vel_Data)
+describe_diff_by_const, Alpha_Vel_Data = filter_data_describe(Alpha_Vel_Data, vel_col, alpha_col)
 print(describe_diff_by_const)
-print(Diffusion_Data.Theory_Vel[0])
-print(find_optimal_constant(describe_diff_by_const, Diffusion_Data, vel_data))
+print(Alpha_Vel_Data.Theory_Vel[0])
+print(find_optimal_constant(describe_diff_by_const, Alpha_Vel_Data, vel_col))
 
 
 print("If the p value is large then the null hypothesis cannot be rejected and there is no evidence of a difference")
