b5c32026bb4b86d2e0502ade3f8232d1c59034bd
Michael Frood <132622476+Frood-for-Thought@users.noreply.github.com>
2024-02-14 00:01:18 -0500

    create loss function derivative

--- ML/Loss_Function.m
+++
@@ -1,8 +0,0 @@
-function [L] = Loss_Function(...
-    Average_Theory_Vel, Calculated_Ave_Vd_Array, alpha)
-    L = 0;
-    for i = 1:length(length(Calculated_Ave_Vd_Array))
-        L = L + (Calculated_Ave_Vd_Array(i) - Average_Theory_Vel)^2;
-    end
-    L = L/length(Calculated_Ave_Vd_Array);
-end
