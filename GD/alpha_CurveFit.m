28abd75361cf17639a199f2e1fd85cf979fea17e
Michael Frood <132622476+Frood-for-Thought@users.noreply.github.com>
2024-03-04 12:10:37 -0500

    created curve fit

--- GD/alpha_CurveFit.m
+++ GD/alpha_CurveFit.m
@@ -22,8 +22,8 @@ Results = [X_data, Y_data, fitresult(X_data)];
 T_res = array2table(Results,'VariableNames',...
     {'X_data','Y_data','Curve_Fit'})
 
-file_title = 'alpha_values_MaxC_%d_Grad_%.6f_curve_fit.xlsx';
-filename = sprintf(file_title, Max_Food_Conc,Grad);
+file_title = 'alpha_values_pos_%d_to_%d_MaxC_%d_Grad_%.6f_curve_fit.xlsx';
+filename = sprintf(file_title, X_data(1), X_data(end), Max_Food_Conc,Grad);
 writetable(T_res,filename,'Sheet',1,'Range','A1')
 
 
@@ -35,10 +35,10 @@ gof
 figure( 'Name', 'Polynomial_Fit' );
 h = plot( fitresult, xData, yData );
 
-hold all
-% Label axes and legend
-m = plot(fitresult(X_data));
-legend( '-DynamicLegend', 'alpha values', 'initial polyfit', 'polynomial regression', 'Location', 'NorthEast' );
+% hold all
+% % Label axes and legend
+% m = plot(fitresult(X_data));
+% legend( '-DynamicLegend', 'alpha values', 'initial polyfit', 'polynomial regression', 'Location', 'NorthEast' );
 
 xlabel deme
 ylabel alpha
