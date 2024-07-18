5e63ccdf98d4b910dadf55ed571dfdb9223c3109
Michael Frood <132622476+Frood-for-Thought@users.noreply.github.com>
2024-07-18 17:03:00 -0400

    Update Tumble_Angle.py

--- GD/Fitting_Python_Code/Tumble_Angle.py
+++ GD/Fitting_Python_Code/Tumble_Angle.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.stats import rv_continuous
+import torch
 
 
 class Tumble_Angle_Distribution(rv_continuous):
@@ -24,3 +25,44 @@ class Angle_Generator(Tumble_Angle_Distribution):
         """
         random_angle_rad = self.rvs()
         return np.degrees(random_angle_rad)
+
+
+# Recalculating for CUDA
+
+
+class TumbleAngleDistribution_cuda:
+    def __init__(self, a=0, b=1, rnd_num_range=1):
+        self.a = a
+        self.b = b
+        self.rnd_num_range = rnd_num_range  # From 0 to rnd_num_range
+
+    def pdf(self, x):
+        """
+        Probability Density Function for the tumble angle.
+        :param x: Angle in Rad.
+        :return: P(x) = 0.5*(1+cos(x))*sin(x)
+        """
+        return 0.5 * (1 + torch.cos(x)) * torch.sin(x)
+
+    def sample(self):
+        """
+        Sample from the custom probability distribution using direct sampling.
+        :return: Tensor of samples in radians.
+        """
+        u = torch.rand(self.rnd_num_range, device='cuda') * (self.b - self.a) + self.a
+        return self.pdf(u)  # Evaluate the PDF at the sampled angles
+
+
+class AngleGenerator_cuda(TumbleAngleDistribution_cuda):
+    def __init__(self):
+        super().__init__(a=0, b=np.pi, rnd_num_range=1)
+
+    def tumble_angle_function_cuda(self):
+        random_angles_rad = self.pdf()
+        return torch.rad2deg(random_angles_rad)
+
+
+# Example usage
+angle_generator = AngleGenerator_cuda()
+angles = angle_generator.tumble_angle_function_cuda()
+print(angles)
