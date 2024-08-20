4a02a49de90ab04eeb2b0d30e932828737c9405d
Michael Frood <132622476+Frood-for-Thought@users.noreply.github.com>
2024-08-20 15:26:39 -0400

    Update Find_Alpha_RawData_Program.py

--- GD/Fitting_Python_Code/Find_Alpha_RawData_Program.py
+++ GD/Fitting_Python_Code/Find_Alpha_RawData_Program.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import torch
 # Import the Tumble Angle Module
 from Tumble_Angle import AngleGenerator_cuda
 # Initialize the angle generator class to select from probability distribution.
 angle_generator = AngleGenerator_cuda()
+from Generate_Dynamic_Data_Points import Norm_Vd_Mean_Data_Generator
 
 
 # Initialization and Food Concentration Calculation.
@@ -17,15 +19,6 @@ for Food_Pos in range(nl):
     Food_Function[Food_Pos] = np.exp(Grad * Food_Pos * DL)
 Ini_Food_Const = Max_Food_Conc / Food_Function[-1]
 xbias = Ini_Food_Const * Food_Function
-
-
-# Generate a new tumble angle.
-next_tumble_angle = angle_generator.tumble_angle_function_cuda(size=10000)
-print(f"Tumble angles: {next_tumble_angle}\nWith mean: {next_tumble_angle.mean()}")
-Start_Angle = 90  # degrees
-Angle = Start_Angle
-
-
 # The theoretical parameters have been pre-calculated to fit onto.
 # These parameters are for:
 #     nl = 101
@@ -62,12 +55,29 @@ fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.title('Drift Velocity and Concentration')
 plt.show()
 
-Pos_Alpha_Array = []
-Ni = 2
-Nj = 100
-for deme_start in range(Ni, Nj + 1):
-    alpha_start = 500
+if __name__ == "__main__":
+    torch.multiprocessing.set_start_method('spawn')  # Required for CUDA tensors
 
-    # Call the ML function to find the most optimum alpha value.
-    Pos_Alpha_Array = Calc_Alpha_ML_Function(
-        Rtroc, F, vd_chemotaxis, alpha_start, deme_start, nl, Angle, Vo_max, xbias, DL, Pos_Alpha_Array)
+    alpha = 200
+    Start_Angle = 90  # degrees
+    Angle = Start_Angle
+    diff = 1.16
+    dt = 0.1
+    max_iter = 20000
+    deme_start = 30
+
+    # Initialize the data generator
+    data_generator = Norm_Vd_Mean_Data_Generator(Rtroc, alpha, Angle, Vo_max, DL, nl, deme_start, diff, dt, max_iter)
+
+    ave_speed = data_generator.simulate_bacterial_movement_cuda()
+    print(ave_speed)
+
+# Pos_Alpha_Array = []
+# Ni = 2
+# Nj = 100
+# for deme_start in range(Ni, Nj + 1):
+#     alpha_start = 500
+#
+#     # Call the ML function to find the most optimum alpha value.
+#     Pos_Alpha_Array = Calc_Alpha_ML_Function(
+#         Rtroc, F, vd_chemotaxis, alpha_start, deme_start, nl, Angle, Vo_max, xbias, DL, Pos_Alpha_Array)
